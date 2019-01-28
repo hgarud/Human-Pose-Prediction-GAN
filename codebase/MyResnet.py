627135867736803d2f403cab260b3b0f0ca36aa7
hgarud <hgarud@ncsu.edu>
2019-01-28 11:59:14 -0500

    minor changes

--- codebase/MyResnet.py
+++ codebase/MyResnet.py
@@ -224,14 +224,15 @@ if __name__ == '__main__':
 
     # Reconstruction + KL divergence losses summed over all elements and batch
     def loss_function(recon_x, x, mu, logvar):
-        # BCE = F.binary_cross_entropy(recon_x, x.view(-1, 26), reduction='elementwise_mean')
+        # BCE = F.binary_cross_entropy(recon_x, x.view((-1, 3, 256, 256)), reduction='elementwise_mean')
         BCE = F.mse_loss(recon_x, x.view((-1, 3, 256, 256)), reduction='elementwise_mean')
         # see Appendix B from VAE paper:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
+        print("BCE: ", BCE)
+        print("KLD: ", KLD)
         return BCE + KLD
 
     batch_size = 32
