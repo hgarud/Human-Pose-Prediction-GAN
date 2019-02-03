bc4eaa46668db72f9f85b3347dcac19fe4da1a87
hgarud <hgarud@ncsu.edu>
2019-02-03 13:15:06 -0500

    added secondary utility script for transformer with key changes for this particular use case

---
+++ codebase/IronHide.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+from BumbleBee import subsequent_mask
+
+
+class NoamOpt(object):
+    """Optim wrapper that implements rate."""
+    def __init__(self, model_size, factor, warmup, optimizer):
+        self.optimizer = optimizer
+        self._step = 0
+        self.warmup = warmup
+        self.factor = factor
+        self.model_size = model_size
+        self._rate = 0
+
+    def step(self):
+        """Update parameters and rate"""
+        self._step += 1
+        rate = self.rate()
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+
+    def rate(self, step = None):
+        """Implement `lrate` above"""
+        if step is None:
+            step = self._step
+        return self.factor * \
+            (self.model_size ** (-0.5) *
+            min(step ** (-0.5), step * self.warmup ** (-1.5)))
+
+def get_std_opt(model):
+    return NoamOpt(model.src_embed[0].d_model, 2, 4000,
+            torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
+
+
+class LabelSmoothing(nn.Module):
+    "Implement label smoothing."
+    def __init__(self, size, smoothing=0.0):
+        super(LabelSmoothing, self).__init__()
+        self.criterion = nn.KLDivLoss(size_average=False)
+        # self.padding_idx = padding_idx
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.size = size
+        self.true_dist = None
+
+    def forward(self, x, target):
+        assert x.size(1) == self.size
+        true_dist = x.data.clone()
+        true_dist.fill_(self.smoothing / (self.size - 2))
+        true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        # true_dist[:, self.padding_idx] = 0
+        # mask = torch.nonzero(target.data == self.padding_idx)
+        # if mask.dim() > 0:
+        #     true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        self.true_dist = true_dist
+        return self.criterion(x, Variable(true_dist, requires_grad=False))
+
+
+class SimpleLossCompute:
+    "A simple loss compute and train function."
+    def __init__(self, generator, criterion, opt=None):
+        self.generator = generator
+        self.criterion = criterion
+        self.opt = opt
+
+    def __call__(self, x, y):
+        x = self.generator(x)
+        loss = self.criterion(x.contiguous().view(-1, x.size(-1)),
+                              y.contiguous().view(-1))
+        loss.backward()
+        if self.opt is not None:
+            self.opt.step()
+            self.opt.optimizer.zero_grad()
+        return loss.data[0] * norm
+
+
+class Batch(object):
+    """Object for holding a batch of data with mask during training."""
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        # self.src_mask = (src != pad).unsqueeze(-2)
+        self.src_mask = None
+        if trg is not None:
+            self.trg = trg[:, :, :]
+            # self.trg_y = trg[:, 1:]
+            self.trg_mask = self.make_std_mask(self.trg)
+            # self.ntokens = (self.trg_y != pad).data.sum()
+
+    @staticmethod
+    def make_std_mask(tgt, pad=None):
+        """Create a mask to hide padding and future words."""
+        # tgt_mask = (tgt != pad).unsqueeze(-2)
+        tgt_mask = Variable(subsequent_mask(tgt.size(1)).type_as(tgt.data))
+        return tgt_mask
